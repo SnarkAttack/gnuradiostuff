d9f9864894e3079ce15f635cf90d7bea12b07d0c
KC <kc@thekarmaproject.net>
2014-12-07 12:37:04 -0500

    Added xkcd image and fil-input testing grc file

--- top_block.py
+++ top_block.py
@@ -2,193 +2,61 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Top Block
-# Generated: Fri Dec  5 12:14:48 2014
+# Generated: Sun Dec  7 12:27:22 2014
 ##################################################
 
-from gnuradio import analog
 from gnuradio import blocks
-from gnuradio import channels
-from gnuradio import digital
 from gnuradio import eng_notation
-from gnuradio import filter
 from gnuradio import gr
-from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
-from gnuradio.fft import window
 from gnuradio.filter import firdes
-from gnuradio.wxgui import fftsink2
-from gnuradio.wxgui import forms
 from grc_gnuradio import blks2 as grc_blks2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
-import numpy
 import wx
 
 class top_block(grc_wxgui.top_block_gui):
 
     def __init__(self):
         grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
-        self.noise_volt = noise_volt = 10e-3
-        self.mult_const = mult_const = 1
-        self.cutoff = cutoff = 10.7e3
 
         ##################################################
         # Blocks
         ##################################################
-        _noise_volt_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._noise_volt_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	sizer=_noise_volt_sizer,
-        	value=self.noise_volt,
-        	callback=self.set_noise_volt,
-        	label="Noise Voltage",
-        	converter=forms.float_converter(),
-        	proportion=0,
-        )
-        self._noise_volt_slider = forms.slider(
-        	parent=self.GetWin(),
-        	sizer=_noise_volt_sizer,
-        	value=self.noise_volt,
-        	callback=self.set_noise_volt,
-        	minimum=0,
-        	maximum=1,
-        	num_steps=100,
-        	style=wx.SL_HORIZONTAL,
-        	cast=float,
-        	proportion=1,
-        )
-        self.Add(_noise_volt_sizer)
-        _mult_const_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._mult_const_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	sizer=_mult_const_sizer,
-        	value=self.mult_const,
-        	callback=self.set_mult_const,
-        	label="Amplifier",
-        	converter=forms.float_converter(),
-        	proportion=0,
-        )
-        self._mult_const_slider = forms.slider(
-        	parent=self.GetWin(),
-        	sizer=_mult_const_sizer,
-        	value=self.mult_const,
-        	callback=self.set_mult_const,
-        	minimum=0,
-        	maximum=2,
-        	num_steps=100,
-        	style=wx.SL_HORIZONTAL,
-        	cast=float,
-        	proportion=1,
-        )
-        self.Add(_mult_const_sizer)
-        _cutoff_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._cutoff_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	sizer=_cutoff_sizer,
-        	value=self.cutoff,
-        	callback=self.set_cutoff,
-        	label="Cutoff Frequency",
-        	converter=forms.float_converter(),
-        	proportion=0,
-        )
-        self._cutoff_slider = forms.slider(
-        	parent=self.GetWin(),
-        	sizer=_cutoff_sizer,
-        	value=self.cutoff,
-        	callback=self.set_cutoff,
-        	minimum=5e3,
-        	maximum=16e3,
-        	num_steps=100,
-        	style=wx.SL_HORIZONTAL,
-        	cast=float,
-        	proportion=1,
-        )
-        self.Add(_cutoff_sizer)
-        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
-        	self.GetWin(),
-        	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title="Tx",
-        	peak_hold=False,
-        )
-        self.Add(self.wxgui_fftsink2_0_0.win)
-        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
-        	self.GetWin(),
-        	baseband_freq=0,
-        	y_per_div=10,
-        	y_divs=10,
-        	ref_level=0,
-        	ref_scale=2.0,
-        	sample_rate=samp_rate,
-        	fft_size=1024,
-        	fft_rate=15,
-        	average=False,
-        	avg_alpha=None,
-        	title="Rx",
-        	peak_hold=False,
-        )
-        self.Add(self.wxgui_fftsink2_0.win)
-        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
-        	1, samp_rate, cutoff, 100, firdes.WIN_HAMMING, 6.76))
-        self.digital_gfsk_mod_0 = digital.gfsk_mod(
-        	samples_per_symbol=2,
-        	sensitivity=1.0,
-        	bt=0.35,
-        	verbose=False,
-        	log=False,
-        )
-        self.channels_channel_model_0 = channels.channel_model(
-        	noise_voltage=noise_volt,
-        	frequency_offset=0.0,
-        	epsilon=1.0,
-        	taps=(1.0 + 1.0j, ),
-        	noise_seed=42,
-        	block_tags=False
-        )
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_multiply_xx_1 = blocks.multiply_vcc(1)
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((mult_const, ))
+        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(1, gr.GR_MSB_FIRST)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, "/home/cooper/Documents/fall14/enee322/final_project/xkcd.png", False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "output.png", False)
+        self.blocks_file_sink_0.set_unbuffered(True)
         self.blks2_packet_encoder_0 = grc_blks2.packet_mod_b(grc_blks2.packet_encoder(
-        		samples_per_symbol=2,
+        		samples_per_symbol=1,
         		bits_per_symbol=1,
         		preamble="",
         		access_code="",
         		pad_for_usrp=True,
         	),
-        	payload_length=0,
+        	payload_length=512,
         )
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, 10000, 1, 0)
-        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 256, 512)), True)
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_b(grc_blks2.packet_decoder(
+        		access_code="",
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
+        	),
+        )
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_throttle_0, 0), (self.wxgui_fftsink2_0, 0))
-        self.connect((self.analog_random_source_x_0, 0), (self.blks2_packet_encoder_0, 0))
-        self.connect((self.blks2_packet_encoder_0, 0), (self.digital_gfsk_mod_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.channels_channel_model_0, 0))
-        self.connect((self.digital_gfsk_mod_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.low_pass_filter_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.channels_channel_model_0, 0), (self.blocks_multiply_xx_1, 0))
-        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_1, 1))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.wxgui_fftsink2_0_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blks2_packet_encoder_0, 0))
+        self.connect((self.blks2_packet_encoder_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
+        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.blks2_packet_decoder_0, 0))
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_file_sink_0, 0))
 
 
 
@@ -197,38 +65,6 @@ class top_block(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
-        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, 100, firdes.WIN_HAMMING, 6.76))
-
-    def get_noise_volt(self):
-        return self.noise_volt
-
-    def set_noise_volt(self, noise_volt):
-        self.noise_volt = noise_volt
-        self._noise_volt_slider.set_value(self.noise_volt)
-        self._noise_volt_text_box.set_value(self.noise_volt)
-        self.channels_channel_model_0.set_noise_voltage(self.noise_volt)
-
-    def get_mult_const(self):
-        return self.mult_const
-
-    def set_mult_const(self, mult_const):
-        self.mult_const = mult_const
-        self._mult_const_slider.set_value(self.mult_const)
-        self._mult_const_text_box.set_value(self.mult_const)
-        self.blocks_multiply_const_vxx_0.set_k((self.mult_const, ))
-
-    def get_cutoff(self):
-        return self.cutoff
-
-    def set_cutoff(self, cutoff):
-        self.cutoff = cutoff
-        self._cutoff_slider.set_value(self.cutoff)
-        self._cutoff_text_box.set_value(self.cutoff)
-        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, 100, firdes.WIN_HAMMING, 6.76))
 
 if __name__ == '__main__':
     import ctypes
